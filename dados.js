90b57347e97a31ec64605ee03d01843b13921092
JoanaG0mes <joana.gomes2377@gmail.com>
2024-09-08 03:07:09 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ dados.js
@@ -0,0 +1,21 @@
+let dados = [
+  {
+    titulo: "Olinda-PE",
+    descricao: "Olinda é uma cidade histórica localizada no estado de Pernambuco. Patrimônio Cultural da Humanidade pela UNESCO, Olinda é conhecida por sua arquitetura colonial, centros culturais e rica história",
+    link: "https://www.olinda.pe.gov.br/",
+    tags: "histórico cidade"
+  },
+  {
+    titulo: "Porto de Galinhas-PE",
+    descricao: "Porto de Galinhas é um paraíso tropical localizado em Pernambuco, famoso por suas piscinas naturais, praias exuberantes e vida marinha rica. É um destino ideal para quem busca sol, mar e natureza.",
+    link: "https://www.portodegalinhas.pe.gov.br/",
+    tags: "praias piscinas naturais ecoturismo culinária artesanato litoral Pernambuco"
+  },
+  {
+    titulo: "Fernando de Noronha",
+    descricao: "Fernando de Noronha é um arquipélago paradisíaco localizado no Oceano Atlântico, conhecido por suas praias desertas, paisagens exuberantes e vida marinha rica. É um dos destinos mais exclusivos do Brasil.",
+    link: "https://www.fernandodenoronha.pe.gov.br/",
+    tags: "arquipélago praias mergulho ecoturismo vida marinha Pernambuco"
+  }
+];
+
